fd814e01471e10a5280f3596ef0b9174131ad7f9
unknown <crystyanknappverly@gmail.com>
2026-07-13 23:08:34 -0300

    added features and banner responsive

--- src/Pages/Home/Features/Features.jsx
+++ src/Pages/Home/Features/Features.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import styles from './Features.module.css';
+
 import Delivery from '../../../Assets/icons/Delivery.svg?react';
 import Satisfaction from '../../../Assets/icons/Satisfaction.svg?react';
 import Secure from '../../../Assets/icons/ShieldCheck.svg?react';
 
+const features = [
+	{
+		Icon: Delivery,
+		title: 'Free Shipping',
+		text: "Upgrade your style today and get FREE shipping on all orders! Don't miss out.",
+	},
+	{
+		Icon: Satisfaction,
+		title: 'Satisfaction Guarantee',
+		text: 'Shop confidently with our Satisfaction Guarantee: Love it or get a refund.',
+	},
+	{
+		Icon: Secure,
+		title: 'Secure Payment',
+		text: 'Your security is our priority. Your payments are secure with us.',
+	},
+];
+
 const Features = () => {
-	const features = [
-		{
-			icon: <Delivery />,
-			title: 'Free Shipping',
-			text: `Upgrade your style today and get FREE shipping on all orders! Don't miss out.`,
-		},
-		{
-			icon: <Satisfaction />,
-			title: 'Satisfaction Guarantee',
-			text: `Shop confidently with our Satisfaction Guarantee: Love it or get a refund.`,
-		},
-		{
-			icon: <Secure />,
-			title: 'Secure Payment',
-			text: `Your security is our priority. Your payments are secure with us.`,
-		},
-	];
 	return (
-		<section className={`${styles.featuresContainer} container`}>
-			{features.map((item) => (
-				<article className={styles.featureCard} key={item.title}>
-					<div className={styles.icon}>{item.icon}</div>
-					<h3>{item.title}</h3>
-					<p>{item.text}</p>
+		<section className={`${styles.features} container`}>
+			{features.map(({ Icon, title, text }) => (
+				<article className={styles.featureCard} key={title}>
+					<div className={styles.icon}>
+						<Icon />
+					</div>
+
+					<h3>{title}</h3>
+
+					<p>{text}</p>
 				</article>
 			))}
 		</section>
